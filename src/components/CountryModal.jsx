b0d6938d5f37eea9eab515eb8910a01c20f9f623
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2026-04-17 00:58:45 +0200

    [UPD] styles separato

--- src/components/CountryModal.jsx
+++ src/components/CountryModal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
+import "../styles/countrymodal.css";
 
 const CountryModal = ({ show, onHide, country }) => {
   const images = useMemo(() => {
